584c5fe1f86c707a223387b3b45c2164f0b6b1df
girlsday <girlsday@bitgrip.de>
2023-04-27 16:00:01 +0200

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,44 @@
 import { useState } from "react";
-import { RiShareForwardLine } from 'react-icons/all';
 import "./App.css";
-import { Card, Container, MantineProvider, Text, Title, Image, List, Button, Avatar, Flex, Blockquote, Space } from "@mantine/core";
+import { Container, MantineProvider, Text, Image, List, Avatar, Flex, Blockquote, Space, Group, Center } from "@mantine/core";
 import { Carousel } from '@mantine/carousel';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
-      <Container bg={"#E7F5FF"}>
+      <Container bg={"#E7F5FF"} fluid>
         <Container>
           <Space h={50}></Space>
           <Avatar size={200} maw={200} mx="auto" radius="500px" src="/girlsday-portfolio-smilla/GirlsDay-16.jpg" alt="Random image" />
           <Text c=""fw={500} fz={"60px"}>SMILLA</Text>
         </Container>
 
-        <Container>
-
         <Flex
-          mih={50}
-          gap="md"
-          direction="row"
-          wrap="nowrap"
-        >
-        <List>
+      direction={{ base: 'column', sm: 'row' }}
+      gap={{ base: 'sm', sm: 'lg' }}
+      justify={{ sm: 'center' }}
+    >
+      <Group position="center">
+      <List ta="start">
             <List.Item>Gitarre</List.Item>
             <List.Item>Fußball</List.Item>
             <List.Item>Malen</List.Item>
-        </List>
+          </List>
 
-          <Text fz={"17px"}>Ich habe zwei Hunde und verbringe gerne Zeit mit kreativen Dingen.
-         </Text>
+          <Text fz={"17px"}>Ich habe zwei Hunde und verbringe gerne Zeit mit kreativen Dingen.</Text>
+      </Group>
 
-
-        </Flex>
+    </Flex>
+         
           
-        </Container>
-        
-        <Blockquote cite="– Smilla">
+
+     
+        <Center>
+        <Blockquote my={20} cite="– Smilla">
         Der Weg ist das Ziel
     </Blockquote>
+          </Center>  
+
     
     <Carousel
      
